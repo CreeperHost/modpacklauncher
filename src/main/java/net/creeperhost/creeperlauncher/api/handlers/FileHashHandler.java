6c1bf054876c262cab56812cfa3bfdc2cff80342
Aaron Mills <aaron_m@creeperhost.net>
2020-07-04 22:31:35 +0100

    Add filehashes, "fileHash" call

---
+++ src/main/java/net/creeperhost/creeperlauncher/api/handlers/FileHashHandler.java
@@ -0,0 +1,16 @@
+package net.creeperhost.creeperlauncher.api.handlers;
+
+import net.creeperhost.creeperlauncher.Constants;
+import net.creeperhost.creeperlauncher.Settings;
+import net.creeperhost.creeperlauncher.api.data.FileHashData;
+import net.creeperhost.creeperlauncher.util.FileUtils;
+
+import java.io.File;
+
+public class FileHashHandler implements IMessageHandler<FileHashData> {
+    @Override
+    public void handle(FileHashData data) {
+        File file = new File(Settings.settings.getOrDefault("instanceLocation", Constants.INSTANCES_FOLDER_LOC), data.uuid + File.separator + data.filePath);
+        Settings.webSocketAPI.sendMessage(new FileHashData.Reply(data, FileUtils.getHash(file, "MD5"), FileUtils.getHash(file, "SHA-256")));
+    }
+}
